9a29f8f4ddeba614755c16786a92074549041619
alex <ach1@vt.edu>
2018-06-28 11:03:46 -0400

    jQuery AJAX call is broken. :(

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template, json, request
-app = Flask(__name__)
+from flaskext.mysql import MySQL
+from werkzeug import generate_password_hash, check_password_hash
 
+app = Flask(__name__)
+mysql = MySQL()
+
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'TinyMB'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+#Flask Routing
 @app.route("/")
 def main():
     return render_template('index.html')
@@ -11,15 +23,35 @@ def showSignUp():
 
 @app.route('/signUp',methods=['POST'])
 def signUp():
-     # read the posted values from the UI
-    _email = request.form['inputEmail']
-    _password = request.form['inputPassword']
+    try:
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
 
-     # validate the received values
-    if _email and _password:
-        return json.dumps({'html':'<span>All fields good !!</span>'})
-    else:
-        return json.dumps({'html':'<span>Enter the required fields</span>'})
+        # validate the received values
+        if _email and _password:
+
+            # All Good, let's call MySQL
+
+            cursor = mysql.get_db().cursor()
+
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('sp_createUser',(_email,_hashed_password))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close()
+        conn.close()
+
 
 if __name__ == "__main__":
     app.run()
